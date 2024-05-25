e3a11cbf8c564a7c3b907ac2f59b55ee046d4837
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2024-05-24 18:51:06 -0700

    Updated to sqlite3 db

--- tcg_project/settings.py
+++ tcg_project/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'tcg_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST', default='localhost'),
-        'PORT': config('DATABASE_PORT', default='3306'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
