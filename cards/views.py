5e8170f71e6160a2eaa96d15c3687976fb6315d9
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2024-05-24 21:13:58 -0700

    created api endpoint with authentication

--- cards/views.py
+++ cards/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Card
 from .serializers import CardSerializer
 
@@ -6,4 +7,10 @@ from .serializers import CardSerializer
 class CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+    permission_classes = [IsAuthenticated]
 
+    def list(self, request, *args, **kwargs):
+        print("Request Headers:", request.headers)
+        print("User:", request.user)
+        print("Is Authenticated:", request.user.is_authenticated)
+        return super().list(request, *args, **kwargs)
