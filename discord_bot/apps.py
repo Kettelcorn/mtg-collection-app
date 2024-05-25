0a0e52b2bc25368350cfb056b53cb19dbb1f8185
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2024-05-25 11:35:44 -0700

    Updated apps.py to only create one bot instance

--- discord_bot/apps.py
+++ discord_bot/apps.py
@@ -4,14 +4,3 @@ import threading
 
 class DiscordBotConfig(AppConfig):
     name = 'discord_bot'
-
-    def ready(self):
-        if not hasattr(self, 'started'):
-            self.started = True
-            from .bot import run_bot  # Import the bot's run function
-
-            def start_bot():
-                run_bot()
-
-            thread = threading.Thread(target=start_bot)
-            thread.start()
