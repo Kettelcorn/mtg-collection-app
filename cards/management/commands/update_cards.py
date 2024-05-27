e16fa915811239baa3ffc3d7aa2d042c5b668a15
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2024-05-26 17:42:52 -0700

    Added log messages for debugging

--- cards/management/commands/update_cards.py
+++ cards/management/commands/update_cards.py
@@ -10,15 +10,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         download_uri = get_bulk_data_download_uri()
+        logging.info(f'Download URI: {download_uri}')
         if not download_uri:
+            logging.error('Failed to get download URI')
             self.stdout.write(self.style.ERROR('Failed to get download URI'))
             return
 
         bulk_data = download_bulk_data(download_uri)
+        logging.info(f'First 100 bytes of bulk data: {bulk_data[:100]}')
         if not bulk_data:
+            logging.error('Failed to download bulk data')
             self.stdout.write(self.style.ERROR('Failed to download bulk data'))
             return
 
         process_bulk_data(bulk_data)
-
+        logging.info('Successfully updated cards')
         self.stdout.write(self.style.SUCCESS('Successfully updated cards'))
