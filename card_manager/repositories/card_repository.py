d1061917ed936493e1005d86611bdbd259627eba
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2024-06-28 15:13:16 -0700

    Added repository database methods

--- card_manager/repositories/card_repository.py
+++ card_manager/repositories/card_repository.py
@@ -6,10 +6,19 @@ class CardRepository:
     def get_cards_by_collection_and_name(self, collection, card_name):
         return collection.cards.filter(card_name=card_name)
 
-    # Delete all card_manager by collection
+    # Get card by tcg_id and finish
+    def get_card_by_tcg_id_and_finish(self, tcg_id, finish):
+        return Card.objects.get(tcg_id=tcg_id, finish=finish)
+
+    # Delete all cards by collection
     def delete_all_cards_by_collection(self, collection):
         collection.cards.all().delete()
 
     # Create a card
     def create_card(self, card_data):
         return Card.objects.create(**card_data)
+
+    # Update the price of a card
+    def update_card_price(self, card, price):
+        card.price = price
+        card.save()
